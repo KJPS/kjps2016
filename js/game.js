d70f3ff14223788f4dcf9c71d3279ac0c4c7feb2
Janis Gruzis <jgruzis@gmail.com>
2016-06-02 01:19:50 +0300

    Lib upgrade

--- js/game.js
+++ js/game.js
@@ -1,7 +1,11 @@
 window.onload = function(){
-    init();
+    init(800, 600, { backgroundColor: 0xffffff });
     animate(function(){
-        var t = text('Hello world', { fill: 'white', font:'bold 50px Arial' });
-        setPosition(t, 100, 100);
+        var txt = text(100, 100, 'Hello world');
+        var r = rectangle(100, 150, 20, 20, 0xff0000);
+        var c = circle(100, 200, 20, 0x00ff00);
+        var t = triangle(100, 250, 200, 250, 150, 300);
+        var e = elipse(100, 300, 40, 30, 0x0000ff);
+        var i = image(100, 350, 'image/ship.png');
     });
 };
--- js/lib.js
+++ js/lib.js
@@ -8,15 +8,11 @@ var stage;
  *
  * @param {Object} options
  */
-function init(options) {
-    options = !typeof options === "object" ? options : {};
+function init(width, height, options) {
+    width = width ? width : 800;
+    height = height ? height : 600;
 
-    options = Object.assign({
-        width: 800,
-        height: 600
-    }, options);
-
-    renderer = new PIXI.WebGLRenderer(800, 600);
+    renderer = new PIXI.WebGLRenderer(width, height, options);
     document.body.appendChild(renderer.view);
     stage = new PIXI.Container();
 }
@@ -37,31 +33,131 @@ function animate(tick) {
 }
 
 /**
- * Uzzīmēt tekstu. Vairāk skatīt {@link http://pixijs.github.io/docs/PIXI.Text.html|PIXI.Text}.
+ * Uzzīmēt tekstu.
  *
+ * @param {Number} x
+ * @param {Number} y
  * @param {String} text
  * @param {Object} style
  *
  * @return PIXI.Text
  */
-function text(text, style) {
+function text(x, y, text, style) {
     var text = new PIXI.Text(text, style);
+    text.position.set(x, y);
     stage.addChild(text);
 
     return text;
 }
 
 /**
- * Set position to PIXI object.
+ * Draw rectangle.
  *
- * @param {Object} object
  * @param {Number} x
  * @param {Number} y
- *
- * @return {Object}
+ * @param {Number} width
+ * @param {Number} height
+ * @param color
  */
-function setPosition(object, x, y) {
-    object.position.set(x, y);
+function rectangle(x, y, width, height, color) {
+    var g = new PIXI.Graphics();
+    color = color === undefined ? 0x000000 : color;
 
-    return object;
+    g.beginFill(color);
+    g.drawRect(x, y, width, height);
+    g.endFill();
+
+    stage.addChild(g);
+
+    return g;
+}
+
+/**
+ * Draw polygon.
+ *
+ * @param {Number} x1
+ * @param {Number} y1
+ * @param {Number} x2
+ * @param {Number} y2
+ * @param {Number} x3
+ * @param {Number} y3
+ * @param color
+ */
+function triangle(x1, y1, x2, y2, x3, y3, color) {
+    return polygon([
+        x1, y1,
+        x2, y2,
+        x3, y3
+    ], color);
+}
+
+/**
+ * Draw polygon.
+ *
+ * @param {Array} points
+ * @param color
+ */
+function polygon(points, color) {
+    var g = new PIXI.Graphics();
+    color = color === undefined ? 0x000000 : color;
+
+    g.beginFill(color);
+    g.drawPolygon(points);
+    g.endFill();
+
+    stage.addChild(g);
+
+    return g;
+}
+
+/**
+ * Draw circle.
+ *
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} radius
+ * @param color
+ */
+function circle(x, y, radius, color) {
+    var g = new PIXI.Graphics();
+    color = color === undefined ? 0x000000 : color;
+
+    g.beginFill(color);
+    g.drawCircle(x, y, radius);
+    g.endFill();
+
+    stage.addChild(g);
+
+    return g;
+}
+
+/**
+ * Draw elipse.
+ *
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} width
+ * @param {Number} height
+ * @param color
+ */
+function elipse(x, y, width, height, color) {
+    var g = new PIXI.Graphics();
+    color = color === undefined ? 0x000000 : color;
+
+    g.beginFill(color);
+    g.drawEllipse(x, y, width, height);
+    g.endFill();
+
+    stage.addChild(g);
+
+    return g;
+}
+
+function image(x, y, url) {
+    var texture = PIXI.Texture.fromImage(url);
+    var sprite = new PIXI.Sprite(texture);
+    sprite.position.set(x, y);
+    stage.addChild(sprite);
+
+    return sprite;
 }
