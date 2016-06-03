625711f5a8aa86879091fc10368c647d726d574a
Janis Gruzis <jgruzis@gmail.com>
2016-06-04 00:03:51 +0300

    Dragging, click, other stuff

--- js/game.js
+++ js/game.js
@@ -1,11 +1,29 @@
 window.onload = function(){
     init(800, 600, { backgroundColor: 0xffffff });
+
+    var txt = text(100, 100, 'Hello world');
+    var r = rectangle(100, 150, 20, 20, 0xff0000);
+    var c = circle(100, 200, 20, 0x00ff00);
+    var t = triangle(100, 250, 200, 250, 150, 300);
+    var e = elipse(100, 300, 40, 30, 0x0000ff);
+    //var i = image(100, 350, 'image/ship.png');
+
+    draggable(txt);
+    draggable(r);
+    draggable(c);
+    draggable(t);
+    draggable(e);
+
+    onClick(e, function(){
+        console.log('Click');
+    });
+
     animate(function(){
-        var txt = text(100, 100, 'Hello world');
-        var r = rectangle(100, 150, 20, 20, 0xff0000);
-        var c = circle(100, 200, 20, 0x00ff00);
-        var t = triangle(100, 250, 200, 250, 150, 300);
-        var e = elipse(100, 300, 40, 30, 0x0000ff);
-        var i = image(100, 350, 'image/ship.png');
+        var now = new Date().getTime();
+        moveBy(r, 1, 1);
+
+        if (isCollision(t, r)) {
+            console.log('Collision');
+        }
     });
 };
--- js/lib.js
+++ js/lib.js
@@ -6,6 +6,8 @@ var stage;
 /**
  * Sākt spēli.
  *
+ * @param {Number} width
+ * @param {Number} height
  * @param {Object} options
  */
 function init(width, height, options) {
@@ -58,14 +60,17 @@ function text(x, y, text, style) {
  * @param {Number} width
  * @param {Number} height
  * @param color
+ *
+ * @return PIXI.Graphics
  */
 function rectangle(x, y, width, height, color) {
     var g = new PIXI.Graphics();
     color = color === undefined ? 0x000000 : color;
 
     g.beginFill(color);
-    g.drawRect(x, y, width, height);
+    g.drawRect(0, 0, width, height);
     g.endFill();
+    g.position.set(x, y);
 
     stage.addChild(g);
 
@@ -82,6 +87,8 @@ function rectangle(x, y, width, height, color) {
  * @param {Number} x3
  * @param {Number} y3
  * @param color
+ *
+ * @return PIXI.Graphics
  */
 function triangle(x1, y1, x2, y2, x3, y3, color) {
     return polygon([
@@ -96,14 +103,29 @@ function triangle(x1, y1, x2, y2, x3, y3, color) {
  *
  * @param {Array} points
  * @param color
+ *
+ * @return PIXI.Graphics
  */
 function polygon(points, color) {
     var g = new PIXI.Graphics();
     color = color === undefined ? 0x000000 : color;
 
+    var minX = 2000000000, minY = 2000000000;
+
+    for (var i = 0; i < points.length; i += 2) {
+        minX = Math.min(minX, points[i]);
+        minY = Math.min(minY, points[i + 1]);
+    }
+
+    for (var i = 0; i < points.length; i += 2) {
+        points[i] -= minX;
+        points[i + 1] -= minY;
+    }
+
     g.beginFill(color);
     g.drawPolygon(points);
     g.endFill();
+    g.position.set(minX, minY);
 
     stage.addChild(g);
 
@@ -117,14 +139,17 @@ function polygon(points, color) {
  * @param {Number} y
  * @param {Number} radius
  * @param color
+ *
+ * @return PIXI.Graphics
  */
 function circle(x, y, radius, color) {
     var g = new PIXI.Graphics();
     color = color === undefined ? 0x000000 : color;
 
     g.beginFill(color);
-    g.drawCircle(x, y, radius);
+    g.drawCircle(0, 0, radius);
     g.endFill();
+    g.position.set(x, y);
 
     stage.addChild(g);
 
@@ -139,20 +164,32 @@ function circle(x, y, radius, color) {
  * @param {Number} width
  * @param {Number} height
  * @param color
+ *
+ * @return PIXI.Graphics
  */
 function elipse(x, y, width, height, color) {
     var g = new PIXI.Graphics();
     color = color === undefined ? 0x000000 : color;
 
     g.beginFill(color);
-    g.drawEllipse(x, y, width, height);
+    g.drawEllipse(0, 0, width, height);
     g.endFill();
+    g.position.set(x, y);
 
     stage.addChild(g);
 
     return g;
 }
 
+/**
+ * Draw image.
+ *
+ * @param {Number} x
+ * @param {Number} y
+ * @param {String} url
+ *
+ * @return PIXI.Sprite
+ */
 function image(x, y, url) {
     var texture = PIXI.Texture.fromImage(url);
     var sprite = new PIXI.Sprite(texture);
@@ -160,4 +197,122 @@ function image(x, y, url) {
     stage.addChild(sprite);
 
     return sprite;
+}
+
+/**
+ * Add click event to graphics element.
+ *
+ * @param {PIXI.Graphics} g
+ * @param {Function} fn
+ *
+ * @return {PIXI.Graphics}
+ */
+function onClick(g, fn) {
+    g.interactive = true;
+    g.on('click', fn);
+
+    return g;
+}
+
+/**
+ * Move element to position.
+ *
+ * @param {PIXI.Graphics} g
+ * @param {Number} x
+ * @param {Number} y
+ *
+ * @return {PIXI.Graphics}
+ */
+function move(g, x, y) {
+    g.position.set(x, y);
+
+    return g;
+}
+
+/**
+ * Move element by position.
+ *
+ * @param {PIXI.Graphics} g
+ * @param {Number} x
+ * @param {Number} y
+ *
+ * @return {PIXI.Graphics}
+ */
+function moveBy(g, x, y) {
+    return move(g, g.position.x + x, g.position.y + y);
+}
+
+/**
+ * Remove element.
+ *
+ * @param {PIXI.Graphics} g
+ *
+ * @return {PIXI.Graphics}
+ */
+function remove(g) {
+    stage.removeChild(g);
+
+    return g;
+}
+
+/**
+ * Detect element collision.
+ *
+ * @param {PIXI.Graphics} g1
+ * @param {PIXI.Graphics} g2
+ *
+ * @return {Boolean}
+ */
+function isCollision(g1, g2) {
+    var r1 = g1.getBounds();
+    var r2 = g2.getBounds();
+
+    return r1.x <= r2.x + r2.width && r1.x + r1.width >= r2.x &&
+        r1.y <= r2.y + r2.height && r1.y + r1.height >= r2.y;
+}
+
+/**
+ * Enable draggable element.
+ *
+ * @param {PIXI.Graphics} g
+ *
+ * @return {PIXI.Graphics}
+ */
+function draggable(g) {
+    g.interactive = true;
+    var dragData = null;
+
+    function onDragStart(e) {
+        console.log('Drag start');
+        dragData = e.data;
+    }
+
+    function onDragMove() {
+        console.log('Drag move');
+        if (dragData) {
+            var newPosition = dragData.getLocalPosition(stage);
+            g.position.set(newPosition.x, newPosition.y);
+        }
+    }
+
+    function onDragEnd() {
+        console.log('Drag end');
+        dragData = null;
+    }
+
+    g
+        // Drag start
+        .on('mousedown', onDragStart)
+        .on('touchstart', onDragStart)
+        // Drag move
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove)
+        // Drag end
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+    ;
+
+    return g;
 }
